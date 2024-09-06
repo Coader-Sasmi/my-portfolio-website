abe0e1067a349b6575fdec8d032afe94a8a87d48
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2024-09-06 12:05:34 +0530

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="m-6 rounded-lg p-3 font-semibold text-gray-500 text-lg text-center shadow uppercase">
+      This website design is in progress.
+    </div>
+  );
 }
