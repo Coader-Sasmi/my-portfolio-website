584d482b9366350c8bcae9399c53321aceb98582
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2026-09-05 15:13:36 +0530

    google index hide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Sasmita Mahanta | Personal Portfolio",
-  description: "Created by Sasmita Mahanta",
+    title: "Sasmita Mahanta | Personal Portfolio",
+    description: "Created by Sasmita Mahanta",
+   robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
